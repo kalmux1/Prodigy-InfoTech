9bb2f9df3056bc2725c11eb40ed07bf114db1bbe
kalmux1 <kal.mux.cyber@gmail.com>
2024-08-20 17:40:39 +0530

    PassCheck

--- PRODIGY_CS_03/passcheck.py
+++ PRODIGY_CS_03/passcheck.py
@@ -1,2 +1,139 @@
 # This python tool will check your password strength 
 # By kalmux
+
+import re
+from tkinter import *
+
+path = r"D:\GITHUB\Prodigy-InfoTech\PRODIGY_CS_03\10-million-password-list-top-1000000.txt"
+
+def passleak(password):
+    global leakscore
+    leakscore = 0
+    with open (path, 'r') as f:
+        passwords = f.read().splitlines()
+
+    if password in passwords:
+        print("Password found in data leak.")
+        return True
+    leakscore += 1
+    return False
+
+
+def lengthcheck(password):
+    score = 0
+    length = len(password)
+    if length < 8:
+        score = 0
+    if length >= 8:
+        score += 1
+    if length > 12:
+        score += 1
+    if length > 16:
+        score += 1
+    if length > 20:
+        score += 1
+
+    return score
+
+def charcheck(password):
+    score = 0
+    has_upper = re.search(r'[A-Z]', password)
+    has_lower = re.search(r'[a-z]', password)
+    has_digit = re.search(r'[0-9]', password)
+    has_special = re.search(r'[!@#$%^&*(),.?":{}|<>]', password)
+
+    if has_upper:
+        score += 1
+
+    if has_lower:
+        score += 1
+
+    if has_digit:
+        score += 1
+    
+    if has_special:
+        score += 1
+    
+
+    return score
+
+def repetition(password):
+    # Regex to find any character repeated more than two times consecutively
+    pattern = r'(.)\1{2,}'
+    match = re.search(pattern, password)
+    
+    if match:
+        status_lable.config(text=f"Consecutive character repetition found: {match.group()}")
+        return 0
+    return 1 
+    
+
+
+
+def main():
+    password = passwordvar.get()
+    if password == "" :
+        status_lable.config(text="No Password Entered")
+        return
+    if lengthcheck(password) == 0:
+        status_lable.config(text="Password Length is too short !")
+        return
+    if  repetition(password) == 0:
+        return
+    if not passleak(password):
+        length_score = lengthcheck(password)
+        char_score = charcheck(password)
+        repetition_score = repetition(password)
+        total_score = length_score + char_score + leakscore + repetition_score
+        print(f"Password strength score: {total_score}")
+
+        if total_score <= 3:
+            status_lable.config(text="Very Weak")
+        elif total_score > 3 and total_score <= 5:
+            status_lable.config(text="Weak")
+        elif total_score > 5 and total_score <= 7:
+            status_lable.config(text="Moderate")
+        elif total_score > 7:
+            status_lable.config(text="Strong")
+    else:
+        status_lable.config(text="Password found in data leak")
+        
+
+
+base = Tk()
+base.geometry("450x250")
+base.title("Passify")
+
+head_frame = Frame(base)
+head_frame.pack(side=TOP,anchor="center",pady=10)
+
+head_lable = Label(head_frame,text="Welcome To Passify",font=("Times New Roman", 20, "bold"))
+head_lable.grid(row=0,column=0)
+head_title = Label(head_frame,text="Password Strength Checker",font=("Times New Roman", 10, "bold"))
+head_title.grid(row=1,column=0)
+
+pass_frame = Frame(base)
+pass_frame.pack(side=TOP,anchor="nw",padx=25,pady=10)
+
+pass_lable = Label(pass_frame,text="Password:",font=("Times New Roman", 12))
+pass_lable.grid(row=0,column=0)
+
+passwordvar = StringVar()
+passwordvar.set("")
+
+password_entry = Entry(pass_frame,textvariable=passwordvar,font=("Times New Roman", 12),width=40)
+password_entry.grid(row=0,column=1,padx=3)
+
+check_frame = Frame(base)
+check_frame.pack(side=TOP,anchor="center",pady=10)
+
+check_button = Button(check_frame,text="Check",font=("Times New Roman", 12, "bold"),command=main,padx=10)
+check_button.pack()
+
+status_frame = Frame(base)
+status_frame.pack(side=TOP,anchor="center",pady=10)
+
+status_lable = Label(status_frame,text="",font=("Times New Roman", 15, "bold"))
+status_lable.pack()
+
+base.mainloop()
